e00d237b4f7cf61fabd44645213d4bc813ad723a
Computhink-CS126 <cs126@computhink.com.sg>
2026-09-05 06:33:45 +0000

    q1

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,14 @@
-
+let ball;
+function setup(){
+    new Canvas(600,400);
+    background(0); 
+    world.gravity.y = 10;
+    
+}
+function draw(){
+    clear()
+    if (mouse.presses()){
+        fill (random(255),random(255),random(255));
+        ball = new Sprite(mouse.x, mouse.y, 20,"dynamic");
+    }
+}
